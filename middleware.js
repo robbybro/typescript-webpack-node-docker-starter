4bcd29d9079ffc599049e618b29c26a44dcb746b
Robby Brosman <robbybro@extrahop.com>
2018-01-20 17:26:53 -0800

    Move index.html into middleware
    * add meta tag to optimize mobile view

--- middleware.js
+++ middleware.js
@@ -3,13 +3,12 @@ export default (req, res) => {
 		<!doctype html>
 		<html>
 			<head>
-			<title>Typescript Webpack Node Docker Starter</title>
-			<link rel="manifest" href="/manifest.json">
-				${
-                    process.env.NODE_ENV === 'production'
-                        ? '<link rel="stylesheet" href="/bundle.css"></link>'
-                        : ''
-                }
+				<meta charset="utf-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+				<title>Typescript Webpack Node Docker Starter</title>
+				${process.env.NODE_ENV === 'production' ? '<link rel="stylesheet" href="/bundle.css"></link>' : ''}
 			</head>
 			<body>
 				<div id='app'></div>
